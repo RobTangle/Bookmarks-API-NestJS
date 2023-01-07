0e6fd0eeaada30491c0f5234b985dc0547b1647e
Robert0101 <rob.tangled@gmail.com>
2023-01-07 11:27:00 -0300

    Add try catch for duplicated unique email

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { ForbiddenException } from '@nestjs/common/exceptions';
 
 @Injectable()
 export class AuthService {
@@ -10,16 +12,25 @@ export class AuthService {
     // generate the password hash
     const hash = await argon.hash(dto.password);
     // save the new user in the db
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-    });
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
 
-    delete user.hash;
-    //  return the saved user
-    return user;
+      delete user.hash;
+      //  return the saved user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          //este es el código específico de field @unique duplicado
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+    }
   }
   signin() {
     return { msg: "I've signed in" };
