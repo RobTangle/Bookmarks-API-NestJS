d68ba8f106b5b1588a75d26ab2e39a434349037f
Robert0101 <rob.tangled@gmail.com>
2023-01-09 14:18:18 -0300

    Apply morgan()

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
+const morgan = require('morgan');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.use(morgan('common'));
   app.use(helmet());
   app.useGlobalPipes(
     new ValidationPipe({
